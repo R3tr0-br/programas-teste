327b6475d7f08e7275ad87650a794b91f19e097e
GustaZs <gustavo.zils.br@gmail.com>
2025-08-13 20:34:42 -0300

    Add files via upload

---
+++ etapa 2/5.c
@@ -0,0 +1,9 @@
+// receba raio e calcule o raio da circunferencia//
+#include <stdio.h>
+int main(void){
+    const float pi = 3.14;
+    int r;
+    printf("Digite o raio da circuferencia: ");
+    scanf("%d", &r);
+    printf("%d", r**);
+} 
