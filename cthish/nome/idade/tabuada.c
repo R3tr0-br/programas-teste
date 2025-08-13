e94936901aee91b4a091f2b98ed4ef2815b83b36
GustaZs <gustavo.zils.br@gmail.com>
2025-08-13 19:26:34 -0300

    Add files via upload

--- cthish/nome/idade/tabuada.c
+++ cthish/nome/idade/tabuada.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
-int main(void)
-{
-    printf("2X1 = 2 \n 2x2 = 4 \n 2X3 = 6 \n 2X4 = 8 \n 2X5 = 10 \n 2x6 = 12 \n 2X7 = 14 \n 2X8 = 16 \n 2X9 = 18 \n 2X10 = 20");
+int main() {
+    int x = 2;
+
+    printf ("Tabuada do 2!\n");
+    
+    do{
+        printf ("Tabuada: %d \n", x);
+        x+=2;
+        
+    } 
+    while (x <= 20);
     return 0;
 }
